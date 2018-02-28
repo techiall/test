36c69eb8f46a7088ed28ca2d1dd69c77742225e1
techial <techial1042@gmail.com>
2018-02-28 19:05:18 +0800

    Update ja.cpp

--- ja.cpp
+++ ja.cpp
@@ -1,50 +1,100 @@
-#include <iostream>
-#include <string>
-using namespace std;
+#include <stdio.h>
+#include <string.h>
 
-const string delete_java_path[] = {
-	"wmic ENVIRONMENT where \"name='JAVA_HOME'\" delete",
-	"wmic ENVIRONMENT where \"name='CLASS_PATH'\" delete"
-};
+//#define DEBUG
 
-string set_path[] = {
+void delect_system_variables(const char *name)
+{
+	char cmd[300] = {0};
+	sprintf(cmd, "wmic ENVIRONMENT where \"name='%s'\" delete", name);
 
-	"wmic ENVIRONMENT create name=\"JAVA_HOME\","
-	"username=\"<system>\","
-	"VariableValue=\""
-	,
+#ifdef DEBUG
+	printf("%s\n", cmd);
+#else
+	system(cmd);
+#endif // DEBUG
 
-	"wmic ENVIRONMENT create name=\"CLASS_PATH\","
-	"username=\"<system>\","
-	"VariableValue=\""
-	".;%JAVA_HOME%\\lib\\tools.jar;"
-	"%JAVA_HOME%\\lib\\dt.jar;\""
-	,
+}
 
-	"wmic ENVIRONMENT where \"name='Path' and username='<system>'\" "
-	"set VariableValue=\""
-	"%Path%;%JAVA_HOME%\\jre\\bin;%JAVA_HOME%\\bin\""
-	,
-};
+void create_system_variables(const char *name, const char *value, 
+			     const char *use = "system")
+{
+	char cmd[300] = {0};
+	sprintf(cmd,
+		"wmic ENVIRONMENT create name=\"%s\","
+		"username=\"<%s>\","
+		"VariableValue=\"%s\"",
+		name, use, value
+	);
+#ifdef DEBUG
+	printf("%s\n", cmd);
+#else
+	system(cmd);
+#endif // DEBUG;
+
+}
+
+void set_system_variables(const char *name, const char *value, 
+			  const char *use = "system")
+{
+	char cmd[300] = {0};
+	sprintf(cmd,
+		"wmic ENVIRONMENT where \"name='%s' and username='<%s>'\" "
+		"set VariableValue=\"%s\"",
+		name, use, value
+	);
+#ifdef DEBUG
+	printf("%s\n", cmd);
+#else
+	system(cmd);
+#endif // DEBUG
+
+}
+
+void config_java()
+{
+#ifdef DEBUG
+	char java_home_value[50] = "C:\\Program Files\\Java\\jdk1.8.0_141";
+#else
+	char java_home_value[50];
+	scanf("%s", java_home_value);
+#endif
+
+	int len = strlen(java_home_value);
+	if (java_home_value[len - 1] == '\\') {
+		java_home_value[len - 1] == '\0';
+	}
+
+	const char *java_home_name = "JAVA_HOME";
+	const char *class_path_name = "CLASS_PATH";
+
+	delect_system_variables(java_home_name);
+	delect_system_variables(class_path_name);
+
+	create_system_variables(java_home_name, java_home_value);
+
+	char class_path_value[100] = {0};
+	sprintf(class_path_value, ".;%%%s%%\\lib\\tools.jar;%%%s%%\\lib\\dt.jar;", java_home_name, java_home_name);
+	create_system_variables(class_path_name, class_path_value);
+
+	const char *path_name = "Path";
+	char path_value[100] = {0};
+	sprintf(path_value, "%%%s%%;%%%s%%\\jre\\bin;%%%s%%\\bin", path_name, java_home_name, java_home_name);
+	set_system_variables(path_name, path_value);
+}
 
 int main(void)
 {
-#ifdef _DEBUG
-	freopen("in.txt", "r", stdin);
-#endif // _DEBUG
+#ifndef DEBUG
+	puts("右键管理员身份打开");
+#endif // DEBUG
 
-	cout << "右键管理员身份打开\n";
-	cout << "输入 Java 安装目录：";
-	string tmp;
-	getline(cin, tmp);
-	if (tmp[tmp.length() - 1] == '\\')
-		tmp.pop_back();
-
-	set_path[0].append(tmp).append("\"");
-	for (int i = 0; i < sizeof(delete_java_path) / sizeof(*delete_java_path); i++)
-		system(delete_java_path[i].c_str());
-
-	for (int i = 0; i < sizeof(set_path) / sizeof(*set_path); i++)
-		system(set_path[i].c_str());
+	config_java();
 	return 0;
 }
+
+/*
+wmic ENVIRONMENT create name="JAVA_HOME",username="<system>",VariableValue="D:\Program Files\Java\jdk1.8.0_141"
+wmic ENVIRONMENT create name="CLASS_PATH",username="<system>",VariableValue=".;%JAVA_HOME%\lib\tools.jar;%JAVA_HOME%\lib\dt.jar;"
+wmic ENVIRONMENT where "name='Path' and username='<system>'" set VariableValue="%Path%;%JAVA_HOME%\jre\bin;%JAVA_HOME%\bin"
+*/
